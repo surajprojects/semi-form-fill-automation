f78032af0129aab3f051855e15cceb5d8ea67d4b
TIGER <suraj23082002@gmail.com>
2025-08-28 17:12:32 +0530

    fixed the savelogin terminal open bug

--- saveLogin.ts
+++ saveLogin.ts
@@ -2,16 +2,25 @@ import { chromium } from "playwright";
 
 async function saveLogin() {
     const browser = await chromium.launch({ headless: false, args: ["--start-maximized"], });
-    const context = await browser.newContext();
+    const context = await browser.newContext({ viewport: null });
     const page = await context.newPage();
 
     await page.goto("url_here");
 
-    console.log("👉 Login manually, then press Enter...");
-    await new Promise<void>((resolve) => process.stdin.once("data", () => resolve()));
+    console.log("👉 Login manually, then press Enter in terminal...");
+    await new Promise<void>((resolve) => {
+        process.stdin.once("data", () => {
+            process.stdin.pause();
+            resolve();
+        });
+    });
 
-    await context.storageState({ path: "auth.json" });
-    await browser.close();
+    try {
+        await context.storageState({ path: "auth.json" });
+        await browser.close();
+    } catch (err) {
+        console.log("⚠️ Browser was closed before saving auth:", err);
+    }
 };
 
 saveLogin();
